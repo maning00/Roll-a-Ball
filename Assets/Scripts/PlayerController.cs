710a2511090485b820e0eec24e029e01adff2f77
MA NING <ningma1997@gmail.com>
2019-04-09 15:04:38 +0800

    commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text Counttext;
+    public Text wintext;
+    private Rigidbody rb;
+    private int count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        wintext.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+            other.gameObject.SetActive(false);
+        count = count + 1;
+        SetCountText();
+    }
+    void SetCountText()
+    {
+        Counttext.text = "count: " + count.ToString();
+        if (count >= 7)
+        {
+            wintext.text = "YOU WIN";
+        }
+    }
+
+}
